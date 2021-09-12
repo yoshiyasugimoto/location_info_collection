bdcdd0f1c729c39d4a47d64a8ab7ee6e1ae5acf5
yoshiyasugimoto <gabesaiti@gmail.com>
2021-09-13 08:38:50 +0900

    データ構造の変更

--- location-info-collection/handler.py
+++ location-info-collection/handler.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timezone, timedelta
 import json
 import os
 
 import boto3
+from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.getenv("DYNAMODB_TABLE"))
@@ -10,9 +12,14 @@ table = dynamodb.Table(os.getenv("DYNAMODB_TABLE"))
 def post(event, context):
     body: dict = json.loads(event['body'])
     location = body['location']
+
+    convert_datetime_utc = datetime.fromtimestamp(body['timestamp'])
+    convert_datetime_jst = convert_datetime_utc - timedelta(hours=9)
+    date: str = convert_datetime_jst.strftime('%Y-%m-%d')
     table.put_item(
         Item={
             'user_id': body['user_id'],
+            'date': date,
             'timestamp': body['timestamp'],
             'lat_north_south': location['lat_north_south'],
             'latitude': location['latitude'],
